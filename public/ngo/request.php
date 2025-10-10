fabe814839c3e1f1d1f5eab756aa8939c4073350
Akash Biswas <n12371661@qut.edu.au>
2025-10-11 01:05:52 +1000

    final submission

--- public/ngo/request.php
+++ public/ngo/request.php
@@ -97,7 +97,7 @@ if (!empty($requestedFoodIds)) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="<?= $base_url ?>public/css/dashboard.css">
+<link rel="stylesheet" href="<?= BASE_URL ?>public/css/dashboard.css">
 <title>Requested Meals - <?= htmlspecialchars($_SESSION['user_name']); ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
